1385341509476585f01dca0e23d53d090e9152b6
Rhys Campbell <rhys.james.campbell@googlemail.com>
2022-11-15 20:08:46 +0100

    Add parameters

--- main.tf
+++ main.tf
@@ -23,3 +23,24 @@ resource "aws_db_instance" "rds1" {
 
     snapshot_identifier      = local.snapshot_identifier   
 }
+
+resource "aws_ssm_parameter" "endpoint" {
+    name        = "/test/rds/endpoint"
+    description = "Endpoint of the active RDS Instance"
+    type        = "String"
+    value       = aws_db_instance.rds1.endpoint
+}
+
+resource "aws_ssm_parameter" "admin_username" {
+    name        = "/test/rds/username"
+    description = "Username of RDS Instance"
+    type        = "String"
+    value       = local.username
+}
+
+resource "aws_ssm_parameter" "admin_password" {
+    name        = "/test/rds/password"
+    description = "Password of RDS Instance"
+    type        = "SecureString"
+    value       = local.password
+}
